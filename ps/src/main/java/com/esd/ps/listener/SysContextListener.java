22be208f7b7701753f158d286633f7af93b7658d
vchenv <violetregret@yeah.net>
2015-02-09 12:28:14 +0800

    timer停止

--- ps/src/main/java/com/esd/ps/listener/SysContextListener.java
+++ ps/src/main/java/com/esd/ps/listener/SysContextListener.java
@@ -1,63 +1,71 @@
-package com.esd.ps.listener;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-
-import com.esd.ps.util.InsertSalary;
-/**
- * 启动tomcat监听器
- * @author chen
- *
- */
-public class SysContextListener implements ServletContextListener {
-	private Timer t = null;
-	/**
-	 * 启动
-	 */
-	public void contextInitialized(ServletContextEvent sce) {
-		// 一天的毫秒数
-		 long daySpan = 24 * 60 * 60 * 1000;
-		 // 规定的每天时间15:33:30运行
-		 final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd '23:30:00'");
-		 // 首次运行时间
-		 Date startTime = null;
-		try {
-			startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sdf.format(new Date()));
-		} catch (ParseException e) {
-			
-			e.printStackTrace();
-		}		 
-		 // 如果今天的已经过了 首次运行时间就改为明天
-		 if(System.currentTimeMillis() > startTime.getTime()){
-		  startTime = new Date(startTime.getTime() + daySpan);
-		 }				 
-		 //Timer t = new Timer();
-		 t  = new Timer();
-		 TimerTask task = new TimerTask(){
-		  @Override
-		  public void run() {
-			   // 要执行的代码
-			  InsertSalary in = new InsertSalary();
-			  in.insertSalaryFromWorkerRecord();
-			  }
-		 };
-		 // 以每24小时执行一次
-		 t.scheduleAtFixedRate(task, startTime, daySpan);
-		 sce.getServletContext().log("定时器已启动"); 
-	}
-
-	/**
-	 * 关闭
-	 */
-	public void contextDestroyed(ServletContextEvent sce) {
-		t.cancel();
-		sce.getServletContext().log("定时器销毁");
-	}
-
-}
+//package com.esd.ps.listener;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//import javax.servlet.ServletContextEvent;
+//import javax.servlet.ServletContextListener;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.esd.db.service.SalaryService;
+//import com.esd.ps.util.InsertSalary;
+//
+///**
+// * 启动tomcat监听器
+// * @author chen
+// *
+// */
+//public class SysContextListener implements ServletContextListener {
+//
+//	private Timer t = null;
+//	/**
+//	 * 启动
+//	 */
+//	public void contextInitialized(ServletContextEvent sce) {
+//		// 一天的毫秒数
+//		 long daySpan = 24 * 60 * 60 * 1000;
+//		 // 规定的每天时间15:33:30运行
+//		 final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd '15:14:00'");
+//		 // 首次运行时间
+//		 Date startTime = null;
+//		try {
+//			startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sdf.format(new Date()));
+//		} catch (ParseException e) {
+//			
+//			e.printStackTrace();
+//		}		 
+//		 // 如果今天的已经过了 首次运行时间就改为明天
+//		 if(System.currentTimeMillis() > startTime.getTime()){
+//		  startTime = new Date(startTime.getTime() + daySpan);
+//		 }				 
+//		 //Timer t = new Timer();
+//		 t  = new Timer();
+//		 TimerTask task = new TimerTask(){
+//		 
+//		  public void run() {
+//			  System.out.println("进来了"); 
+//			   // 要执行的代码
+//			  //SalaryController in = new SalaryController();
+//			  InsertSalary.insertSalaryFromWorkerRecord(); 
+//		  }
+//		 };
+//		 // 以每24小时执行一次
+//		 t.scheduleAtFixedRate(task, startTime, daySpan);
+//		 sce.getServletContext().log("定时器已启动"); 
+//	}
+//
+//	/**
+//	 * 关闭
+//	 */
+//	public void contextDestroyed(ServletContextEvent sce) {
+//		t.cancel();
+//		sce.getServletContext().log("定时器销毁");
+//	}
+//
+//}
--- ps/src/main/java/com/esd/ps/util/InsertSalary.java
+++ ps/src/main/java/com/esd/ps/util/InsertSalary.java
@@ -1,23 +1,37 @@
-package com.esd.ps.util;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.esd.db.service.SalaryService;
-import com.esd.ps.SalaryController;
-
-/**
- * insert into salary
- * @author chen
- *
- */
-public class InsertSalary {
-	/**
-	 * workerRecord数据插入salary
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(SalaryController.class);
-	private SalaryService salaryService;
-	public void insertSalaryFromWorkerRecord(){
-		int m = salaryService.insertTimer();
-		logger.debug("insertTimer:{}",m);
-	}
-}
+//package com.esd.ps.util;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+//import com.esd.db.service.SalaryService;
+//import com.esd.ps.SalaryController;
+//import com.esd.ps.WorkerController;
+//
+///**
+// * insert into salary
+// * @author chen
+// *
+// */
+//@Controller
+//public class InsertSalary {
+//	/**
+//	 * workerRecord数据插入salary
+//	 */
+//	private static final Logger logger = LoggerFactory.getLogger(SalaryController.class);
+//	@Autowired
+//	private static SalaryService salaryService;
+//	public static void insertSalaryFromWorkerRecord(){
+//		//int m = salaryService.insertTimer();
+//		List<Map<String, Object>> list = salaryService.getWorkerSalaryByWorkerId(25);
+//		logger.debug("insertTimer:{}",list);
+//	}
+//	public static void main(String[] args) {
+//		WorkerController w = new WorkerController();
+//		w.test();
+//	}
+//}
